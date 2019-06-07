e5b1361564dc6f4f4928ea7f98fd15283a2666cb
Alexei Lupan <astenix@gmail.com>
2019-06-07 22:38:07 +0300

    Added all files

---
+++ Scoala/Scoala_Mea/Players_list.py
@@ -0,0 +1,9 @@
+# ! /usr/bin/env python
+#  -*- coding: utf-8 -*-
+'''
+Created on 22 дек. 2018 г.
+@author: astenix
+'''
+if __name__ == '__main__':
+	rocket_players = ['Иван', 'Елена', 'Петя', 'Вася', 'Дима', 'Лена', 'Елена']
+	print(rocket_players[2])	
