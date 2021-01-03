5c836a295190de519b7cf9bc9ca6fe7feec084c6
Alexei Lupan <astenix@testitquickly.com>
2021-01-03 20:51:11 +0200

    Files removed

--- Scoala/Scoala_Mea/Players_list.py
+++
@@ -1,9 +0,0 @@
-# ! /usr/bin/env python
-#  -*- coding: utf-8 -*-
-'''
-Created on 22 дек. 2018 г.
-@author: astenix
-'''
-if __name__ == '__main__':
-	rocket_players = ['Иван', 'Елена', 'Петя', 'Вася', 'Дима', 'Лена', 'Елена']
-	print(rocket_players[2])	
