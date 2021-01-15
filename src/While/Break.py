42a2b912412112e3a83e20648056d596037ef4b9
Alexei Lupan <astenix@testitquickly.com>
2021-01-15 04:54:18 +0200

    + Break - second line with comment

--- src/While/Break.py
+++ src/While/Break.py
@@ -12,5 +12,6 @@ if __name__=='__main__':
         name=input()
         # Приводим условие
         if name=='your name':
+            # Просто прервать выполнение wile и перейти к следующей строке
             break
-        print('Ok')
+    print('Ok')
