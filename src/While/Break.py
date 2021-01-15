393fc55780ea7efe2c7363b399a8fd4eadf31d38
Alexei Lupan <astenix@testitquickly.com>
2021-01-15 04:24:11 +0200

    + Break instruction in While

---
+++ src/While/Break.py
@@ -0,0 +1,15 @@
+# ! /usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+'''
+Created on 15 янв. 2021 г.
+@author: astenix
+'''
+
+if __name__=='__main__':
+    while True:
+        print('Type your name.')
+        name=input()
+        if name=='your name':
+            break
+        print('Ok')
